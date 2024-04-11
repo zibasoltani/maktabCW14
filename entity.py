407b35bec1bcf91cbdeee5a5d58821447f077524
ziba <zibasoltani73@gmail.com>
2024-04-11 20:34:33 +0330

    Zeynab.new

--- entity.py
+++ entity.py
@@ -4,21 +4,21 @@ class Entity:
 
 
 class Student(Entity):
-    def __init__(self, name, date_of_birth):
+    def __init__(self, id, name, date_of_birth):
         super().__init__(id)
         self.name = name
         self.date_of_birth = date_of_birth
 
 
 class Course(Entity):
-    def __init__(self, title, number_of_credits):
+    def __init__(self, id, title, number_of_credits):
         super().__init__(id)
         self.title = title
         self.number_of_credits = number_of_credits
 
 
 class Instructor(Entity):
-    def __init__(self, name, specialization, department):
+    def __init__(self, id, name, specialization, department):
         super().__init__(id)
         self.name = name
         self.specialization = specialization
@@ -26,14 +26,13 @@ class Instructor(Entity):
 
 
 class Department(Entity):
-    def __init__(self, name):
+    def __init__(self, id,  name):
         super().__init__(id)
         self.name = name
 
 
-class StudentRegistration(Entity):
+class StudentRegistration:
     def __init__(self, student_id, course_id, registration_date):
-        super().__init__(id)
         self.student = student_id
         self.course = course_id
         self.registration_date = registration_date
